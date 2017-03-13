dbdafbf72bfe15c55228a5d7b4c7ce0cf88f768c
Peng <1498284491@qq.com>
2017-03-13 22:09:39 +0800

    edit

---
+++ testDemo/testDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testDemo
+//
+//  Created by 云 Love on 2017/3/13.
+//  Copyright © 2017年 云 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testDemo/testDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testDemo
+//
+//  Created by 云 Love on 2017/3/13.
+//  Copyright © 2017年 云 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
